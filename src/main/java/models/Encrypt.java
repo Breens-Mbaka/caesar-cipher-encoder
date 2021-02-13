d7bffda95ddd1bd6685155aedce920fffcf97618
Breens-Thomas <mbakabreens@gmail.com>
2021-02-13 17:48:57 +0300

    Create constructor and properties of the class

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -1,8 +1,19 @@
 package models;
 
 public class Encrypt {
+    private int key;
+    private String input;
+    private String output;
+
+    public Encrypt(int key, String input, String output) {
+        this.key = key;
+        this.input = input;
+        this.output = output;
+    }
+
 
     public String encryptInput(String userInput) {
         return userInput;
     }
+
 }
