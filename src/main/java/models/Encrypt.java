a7156a917b3cbf488f4cf3f27e6c6a4802d4afc8
Breens-Thomas <mbakabreens@gmail.com>
2021-02-14 01:57:18 +0300

    Finding the index of each individual letter of user input in the 26 characters of the alphabet

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -4,7 +4,13 @@ import java.lang.reflect.Array;
 
 public class Encrypt {
     public String encryptInput(String userInput) {
+        //find index of each letter of userInput in the standard alphabet
         char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] arrayOfLetters = userInput.toCharArray();
+        for (char letter: arrayOfLetters) {
+            String stringAlphabet = alphabet.toString();
+            int indexLetter = stringAlphabet.indexOf(letter);
+        }
         return userInput;
     }
 
