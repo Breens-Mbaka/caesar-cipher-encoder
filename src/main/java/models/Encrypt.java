56f603899bfc43f6f571e624ea989284e239926e
Breens-Thomas <mbakabreens@gmail.com>
2021-02-14 02:03:37 +0300

    Finding the index at which we swap our letter with another encrypted letter

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -3,6 +3,8 @@ package models;
 import java.lang.reflect.Array;
 
 public class Encrypt {
+    private int key = 3;
+
     public String encryptInput(String userInput) {
         //find index of each letter of userInput in the standard alphabet
         char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
@@ -10,6 +12,9 @@ public class Encrypt {
         for (char letter: arrayOfLetters) {
             String stringAlphabet = alphabet.toString();
             int indexLetter = stringAlphabet.indexOf(letter);
+            //calculate index of encrypted letter in place of user input
+            int encryptedLetterIndex = (indexLetter + key) % 26;
+            int encryptedLetter = alphabet[encryptedLetterIndex];
         }
         return userInput;
     }
