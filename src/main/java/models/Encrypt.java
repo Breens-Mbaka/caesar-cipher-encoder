e855bdad18e9427a48efd5dcfc65923232b7a949
Breens-Thomas <mbakabreens@gmail.com>
2021-02-13 18:31:18 +0300

    Array of alphabet letters that I will use to get the index of the letter to be matched to

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -1,7 +1,10 @@
 package models;
 
+import java.lang.reflect.Array;
+
 public class Encrypt {
     public String encryptInput(String userInput) {
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         return userInput;
     }
 
