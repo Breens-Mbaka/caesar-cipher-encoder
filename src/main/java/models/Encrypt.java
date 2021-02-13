0fd014deb2804f87345d80b3425afd21a5fc15e2
Breens-Thomas <mbakabreens@gmail.com>
2021-02-13 18:19:53 +0300

    Testing output to return correct value based on input

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -1,17 +1,6 @@
 package models;
 
 public class Encrypt {
-    private int key;
-    private String input;
-    private String output;
-
-    public Encrypt(int key, String input, String output) {
-        this.key = key;
-        this.input = input;
-        this.output = output;
-    }
-
-
     public String encryptInput(String userInput) {
         return userInput;
     }
--- src/test/java/models/EncryptTest.java
+++ src/test/java/models/EncryptTest.java
@@ -5,10 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
+    //testing for input to be a string
     @Test
     void testEncrypt_inputIsString_String() {
         Encrypt testEncrypt = new Encrypt();
         String exampleInput = "tom";
         assertEquals(exampleInput, testEncrypt.encryptInput("tom"));
+    }
+
+    //testing for output should be right when given input
+    @Test
+    void testEncrypt_outputGiven() {
+        Encrypt testEncrypt = new Encrypt();
+        String exampleOutput = "wrp";
+        assertEquals(exampleOutput, testEncrypt.encryptInput("tom"));
     }
 }
