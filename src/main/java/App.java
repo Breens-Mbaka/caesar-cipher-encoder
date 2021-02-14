58f295ad1e289429567e68f406113818bd1df0b8
Breens-Thomas <mbakabreens@gmail.com>
2021-02-14 20:53:10 +0300

    Get the input from user the word they want to decrypt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
 import models.Encrypt;
+import java.util.Scanner;
 
 
 public class App {
     public static void main(String[] args){
         Encrypt encryptWord = new Encrypt();
-        System.out.println(encryptWord.encryptInput("tom"));
+        Scanner myTerminal = new Scanner(System.in);
+        System.out.println("What is the word you want to encrypt");
+        String encryptedWord = myTerminal.nextLine();
     }
 }
