09de0c46758fe9f7f681b99bf02ebf02627b4d6c
Breens-Thomas <mbakabreens@gmail.com>
2021-02-14 19:49:28 +0300

    Test to see the encrypted property is only a string

--- src/main/java/models/Decrypt.java
+++ src/main/java/models/Decrypt.java
@@ -2,4 +2,7 @@ package models;
 
 public class Decrypt {
 
+    public String decrypt(String encrypted) {
+        return encrypted;
+    }
 }
--- src/test/java/models/DecryptTest.java
+++ src/test/java/models/DecryptTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecryptTest {
-
+    @Test
+    void testDecrypting_outputIsString_String() {
+        Decrypt testDecrypt = new Decrypt();
+        String exampleDecrypt = "";
+        assertEquals(exampleDecrypt, testDecrypt.decrypt(""));
+    }
 }
