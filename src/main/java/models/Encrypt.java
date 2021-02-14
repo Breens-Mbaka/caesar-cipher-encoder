fa9bbd377f2c81c659af35d5ee2a8460a42d3b60
Breens-Thomas <mbakabreens@gmail.com>
2021-02-14 09:36:56 +0300

    Make suser input to be in lower case so that they are uniform and can be referenced to alpahbets

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -3,6 +3,7 @@ package models;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Encrypt {
     private int key = 3;
@@ -11,8 +12,9 @@ public class Encrypt {
     public String encryptInput(String userInput) {
         //find index of each letter of userInput in the standard alphabet
         String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        userInput = userInput.toLowerCase();
+
         char[] arrayOfLetters = userInput.toCharArray();
-        String output= "";
         for (char letter: arrayOfLetters) {
             for (int index=0 ; index < alphabets.length; index++) {
                 if(alphabets[index].equals(letter)){
